0353e05c749bfab13935049dba1aeed046c6c3b2
Simonne Pinto <pintosimonne@gmail.com>
2021-04-05 18:38:27 +0530

    like button

--- app/src/main/java/com/example/socitastic/daos/PostDao.kt
+++ app/src/main/java/com/example/socitastic/daos/PostDao.kt
@@ -2,7 +2,9 @@ package com.example.socitastic.daos
 
 import com.example.socitastic.models.Post
 import com.example.socitastic.models.User
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.GlobalScope
@@ -11,7 +13,7 @@ import kotlinx.coroutines.tasks.await
 
 class PostDao {
     private val db = FirebaseFirestore.getInstance()
-    private val postCollections = db.collection("posts")
+    val postCollections = db.collection("posts")
     private val auth = Firebase.auth
 
     fun addPost(text: String){
@@ -25,6 +27,27 @@ class PostDao {
             val post = Post(text, user, currentTime)
 
             postCollections.document().set(post)
+        }
+    }
+
+    fun getPostById(postId: String): Task<DocumentSnapshot> {
+        return postCollections.document(postId).get()
+    }
+
+    fun updateLikes(postId: String){
+        GlobalScope.launch {
+            val currentUserId = auth.currentUser!!.uid;
+            val post = getPostById(postId).await().toObject(Post::class.java)!!
+            val isLiked = post.likedBy.contains(currentUserId)
+
+            if(isLiked){
+                post.likedBy.remove(currentUserId)
+            }
+            else{
+                post.likedBy.add(currentUserId)
+            }
+
+            postCollections.document(postId).set(post)
         }
     }
 }
